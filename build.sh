cea76f534c5083b1100b4ff11a4fd0239ad270c2
Smliexx <msmliexx1@gmail.com>
2023-02-10 13:22:34 +0800

    add mult system build script
    Signed-off-by: Smliexx <msmliexx1@gmail.com>

--- build.sh
+++ build.sh
@@ -1,1 +1,12 @@
-go build -ldflags="-s -w" -gcflags="-B"
+
+if [ $1 == "windows" ];then
+	go build -ldflags="-s -w" -gcflags="-B"
+fi
+
+if [ $1 == "linux" ];then
+        GOOS=linux go build -ldflags="-s -w" -gcflags="-B"
+fi
+
+if [ $1 == "darwin" ];then
+	GOOS=darwin go build -ldflags="-s -w" -gcflags="-B"
+fi
